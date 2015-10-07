48e88a93c4364c3fa4033e010575387c7b50c284
mristau-gn <mristau@gracenote.com>
2015-10-06 20:51:31 -0400

    Initial commit

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,6 @@
+class RecipesController < ApplicationController
+  def index
+    @search_term = params[:search] || 'chocolate'
+    @recipes = Recipes.for(@search_term)['recipes']
+  end
+end
---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+ENV['FOOD2FORK_KEY'] = '685811868a4674d2f8a339be66021b33'
+
+
+# Initialize the Rails application.
+Rails.application.initialize!
